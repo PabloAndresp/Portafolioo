8e11e5d0ac22eaf9a473368c51b76c44abfa2a66
Pablo Pavez <pabloandr@live.cl>
2023-08-10 12:06:56 -0400

    inicio

---
+++ index.js
@@ -0,0 +1,7 @@
+var options = {
+    strings: ['Pablo Pavez', 'Desarrollador Web',  'Full Stack'],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop: true
+  };
+var typed = new Typed('.typing', options);
